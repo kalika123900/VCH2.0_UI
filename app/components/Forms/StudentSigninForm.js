0e74dd9988e2680221609d153a423163038bc4d4
Kalika Prasad <kalika.mongoosetech@gmail.com>
2020-03-11 16:43:13 +0530

    ui

--- app/components/Forms/StudentSigninForm.js
+++ app/components/Forms/StudentSigninForm.js
@@ -60,6 +60,39 @@ class StudentSigninForm extends React.Component {
   handleMouseDownPassword = event => {
     event.preventDefault();
   };
+  componentDidMount() {
+    (function () {
+      var e = document.createElement("script");
+      e.type = "text/javascript";
+      e.async = true;
+      e.src = "http://platform.linkedin.com/in.js?async=true";
+      var t = document.getElementsByTagName("script")[0];
+      t.parentNode.insertBefore(e, t)
+    })();
+  }
+  linkedinLogin = () => {
+    window.IN.init({
+      api_key: "81rgkufnb8jcm8"
+    })
+    setTimeout(function () {
+      this.getUserDetails();
+    }.bind(this), 1000);
+    console.log("Loaded")
+  }
+
+  getUserDetails = () => {
+    window.IN.User.authorize(function () {
+      window.IN.API.Profile("me")
+        .fields(["id", "firstName", "lastName", "pictureUrl", "publicProfileUrl"])
+        .result(function (result) {
+          console.log(result);
+          alert("Successfull login from linkedin : " + result.values[0].firstName + " " + result.values[0].lastName);
+        })
+        .error(function (err) {
+          console.log('Import error - Error occured while importing data')
+        });
+    });
+  }
 
   render() {
     const {
@@ -152,6 +185,7 @@ class StudentSigninForm extends React.Component {
                 background: '#2d72b0',
                 color: ' white',
               }}
+              onClick={this.linkedinLogin}
             >
               <LinkedInIcon style={{ marginRight: '10px' }} />
               Continue with LinkedIn
