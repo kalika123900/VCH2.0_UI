413c9ed5d5c5773e6a0904a905bc5487b930ad69
Kalika Prasad <kalika.mongoosetech@gmail.com>
2020-03-16 12:04:13 +0530

    student profile

--- app/components/Forms/StudentDetails/EditPersonalDetails.js
+++ app/components/Forms/StudentDetails/EditPersonalDetails.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { reduxForm } from 'redux-form/immutable';
 import FormControl from '@material-ui/core/FormControl';
 import styles from '../user-jss';
@@ -15,6 +16,8 @@ import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Input from '@material-ui/core/Input';
+import { storeProfileDetails } from 'dan-actions/studentProfileActions';
+import { genderItems, ethnicityItems, nationalityItems } from 'dan-api/apps/profileOption';
 
 // validation functions
 const required = value => (value === null ? 'Required' : undefined);
@@ -24,18 +27,6 @@ const emailValidator = value => (
     : undefined
 );
 
-const ethnicityItems = [
-  'Oliver Hansen',
-  'Van Henry',
-  'April Tucker',
-  'Ralph Hubbard',
-  'Omar Alexander',
-  'Carlos Abbott',
-  'Miriam Wagner',
-  'Bradley Wilkerson',
-  'Virginia Andrews',
-  'Kelly Snyder',
-];
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -50,35 +41,41 @@ const MenuProps = {
 };
 
 class EditPersonalDetails extends React.Component {
-  state = {
-    firstname: '',
-    lastname: '',
-    email: '',
-    phone: '',
-    DOB: '01/02/2000',
-    nationality: '',
-    file: [],
-    ethnicity: [],
+
+
+  handleDOB = date => {
+    const { addInfo } = this.props;
+
+
+    addInfo({ ...this.props, dob: date });
+
   };
 
-  handleDateChange = date => {
-    this.setState({ DOB: date });
-  };
 
   handleChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
+    const { addInfo } = this.props;
+
+
+    addInfo({ ...this.props, [event.target.name]: event.target.value });
+
   };
 
   render() {
     const {
       classes,
+      firstName,
+      lastName,
+      alternateEmail,
+      phoneNumber,
+      dob,
+      nationality,
+      files,
+      ethnicity,
+      gender
     } = this.props;
 
-    const { file, ethnicity, firstname, lastname,
-      DOB, email, phone, nationality
-    } = this.state;
-
     return (
+
       <section className={classes.pageFormWrap}>
         <div>
           <FormControl className={classes.formControl}>
@@ -86,8 +83,8 @@ class EditPersonalDetails extends React.Component {
               label="First Name"
               className={classes.textField}
               type="text"
-              value={firstname}
-              name="firstname"
+              value={firstName}
+              name="firstName"
               margin="normal"
               variant="outlined"
               validate={[required]}
@@ -101,8 +98,8 @@ class EditPersonalDetails extends React.Component {
               label="Last Name"
               className={classes.textField}
               type="text"
-              name="lastname"
-              value={lastname}
+              name="lastName"
+              value={lastName}
               margin="normal"
               variant="outlined"
               validate={[required]}
@@ -116,8 +113,8 @@ class EditPersonalDetails extends React.Component {
               label="Alternative Email"
               className={classes.textField}
               type="email"
-              name="email"
-              value={email}
+              name="alternateEmail"
+              value={alternateEmail}
               autoComplete="email"
               margin="normal"
               variant="outlined"
@@ -132,9 +129,9 @@ class EditPersonalDetails extends React.Component {
               label="Phone Number"
               className={classes.textField}
               type="text"
-              value={phone}
+              value={phoneNumber}
               autoComplete="phone"
-              name="phone"
+              name="phoneNumber"
               margin="normal"
               variant="outlined"
               validate={[required]}
@@ -149,9 +146,9 @@ class EditPersonalDetails extends React.Component {
                 margin="normal"
                 placeholder="Date of Birth"
                 format="dd/MM/yyyy"
-                value={DOB}
-                name="DOB"
-                onChange={this.handleDateChange}
+                value={dob}
+                name="dob"
+                onChange={this.handleDOB}
                 KeyboardButtonProps={{
                   'aria-label': 'change date',
                 }}
@@ -162,22 +159,30 @@ class EditPersonalDetails extends React.Component {
         <div>
           <FormControl className={classes.formControl}>
             <InputLabel
-              htmlFor="select-ethnicity"
+              htmlFor="select-gender"
             >
-              Ethnicity
+              Gender
                   </InputLabel>
             <Select
-              multiple
-              value={ethnicity}
-              name="ethnicity"
+              value={gender}
+              name="gender"
               onChange={e => this.handleChange(e)}
               input={<Input id="select-ethnicity" />}
-              renderValue={selected => selected.join(', ')}
+              renderValue={selected => {
+                var genderName = '';
+                genderItems.map((value, index) => {
+                  if (selected.includes(value)) {
+                    genderName = value;
+                  }
+                });
+                return genderName;
+              }}
               MenuProps={MenuProps}
             >
-              {ethnicityItems.map((item, index) => (
-                <MenuItem key={index} value={item}>
-                  <Checkbox checked={ethnicityItems.indexOf(item) > -1} />
+              {genderItems.map((item) => (
+                (item != '') &&
+                <MenuItem key={item} value={item}>
+                  <Checkbox checked={gender.indexOf(item) > -1} />
                   <ListItemText primary={item} />
                 </MenuItem>
               ))}
@@ -186,36 +191,130 @@ class EditPersonalDetails extends React.Component {
         </div>
         <div>
           <FormControl className={classes.formControl}>
-            <TextField
-              label="Nationality"
-              className={classes.textField}
-              type="text"
+            <InputLabel
+              htmlFor="select-ethnicity"
+            >
+              Ethnicity
+                  </InputLabel>
+            <Select
+              value={ethnicity}
+              name="ethnicity"
+              onChange={e => this.handleChange(e)}
+              input={<Input id="select-ethnicity" />}
+              renderValue={selected => {
+                var ethinicityName = '';
+                ethnicityItems.map((value, index) => {
+                  if (selected.includes(value)) {
+                    ethinicityName = value;
+                  }
+                });
+                return ethinicityName;
+              }}
+              MenuProps={MenuProps}
+            >
+              {ethnicityItems.map((item, index) => (
+                (item != '') &&
+                <MenuItem key={index} value={item}>
+                  <Checkbox checked={ethnicity.indexOf(item) > -1} />
+                  <ListItemText primary={item} />
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+        <div>
+          <FormControl className={classes.formControl}>
+            <InputLabel
+              htmlFor="select-nationality"
+            >
+              Nationality
+                  </InputLabel>
+            <Select
               value={nationality}
               name="nationality"
-              margin="normal"
-              variant="outlined"
-              validate={[required]}
               onChange={e => this.handleChange(e)}
-            />
+              input={<Input id="select-nationality" />}
+              renderValue={selected => {
+                var nationalityName = '';
+                nationalityItems.map((value, index) => {
+                  if (selected.includes(value)) {
+                    nationalityName = value;
+                  }
+                });
+                return nationalityName;
+              }}
+              MenuProps={MenuProps}
+            >
+              {nationalityItems.map((item, index) => (
+                (item != '') &&
+                <MenuItem key={index} value={item}>
+                  <Checkbox checked={nationality.indexOf(item) > -1} />
+                  <ListItemText primary={item} />
+                </MenuItem>
+              ))}
+            </Select>
           </FormControl>
         </div>
         <div>
           <MaterialDropZone
-            files={file}
+            files={files.toJS()}
             showPreviews
             maxSize={5000000}
             filesLimit={1}
             text="Drag and drop file(s) here to upload CV"
           />
         </div>
-      </section>
+      </section >
 
     );
   }
 }
+const reducerCampaign = 'studentProfile';
 
 EditPersonalDetails.propTypes = {
   classes: PropTypes.object.isRequired,
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
+  alternateEmail: PropTypes.string.isRequired,
+  phoneNumber: PropTypes.string.isRequired,
+  dob: PropTypes.string.isRequired,
+  gender: PropTypes.object.isRequired,
+  ethnicity: PropTypes.object.isRequired,
+  nationality: PropTypes.object.isRequired,
+  files: PropTypes.object.isRequired,
+  // intrestedIndustries: PropTypes.string.isRequired,
+  // intrestedCompanies: PropTypes.number.isRequired,
+  // skills: PropTypes.object.isRequired,
+  // education: PropTypes.string.isRequired,
+  // experience: PropTypes.number.isRequired,
+  addInfo: PropTypes.func.isRequired
 };
 
-export default withStyles(styles)(EditPersonalDetails);
+const mapStateToProps = state => ({
+  firstName: state.getIn([reducerCampaign, 'firstName']),
+  lastName: state.getIn([reducerCampaign, 'lastName']),
+  alternateEmail: state.getIn([reducerCampaign, 'alternateEmail']),
+  phoneNumber: state.getIn([reducerCampaign, 'phoneNumber']),
+  dob: state.getIn([reducerCampaign, 'dob']),
+  gender: state.getIn([reducerCampaign, 'gender']),
+  ethnicity: state.getIn([reducerCampaign, 'ethnicity']),
+  nationality: state.getIn([reducerCampaign, 'nationality']),
+  files: state.getIn([reducerCampaign, 'files']),
+  // intrestedIndustries: state.getIn([reducerCampaign, 'intrestedIndustries']),
+  // intrestedCompanies: state.getIn([reducerCampaign, 'intrestedCompanies']),
+  // skills: state.getIn([reducerCampaign, 'skills']),
+  // education: state.getIn([reducerCampaign, 'education']),
+  // experience: state.getIn([reducerCampaign, 'experience'])
+});
+
+const mapDispatchToProps = dispatch => ({
+  addInfo: bindActionCreators(storeProfileDetails, dispatch)
+});
+
+const StepMapped = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(EditPersonalDetails);
+
+
+export default withStyles(styles)(StepMapped);
