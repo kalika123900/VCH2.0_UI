7a05fc0c6dd23f6eb7dc11f69137190a246b4a53
Kalika Prasad <kalika.mongoosetech@gmail.com>
2020-03-21 12:26:04 +0530

    change spinner and VCH icon

--- app/containers/Tables/MiniCampaignTable.js
+++ app/containers/Tables/MiniCampaignTable.js
@@ -1,11 +1,9 @@
 import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import classNames from 'classnames';
-import Tooltip from '@material-ui/core/Tooltip';
-import Button from '@material-ui/core/Button';
 import { isWidthUp } from '@material-ui/core/withWidth';
 import Typography from '@material-ui/core/Typography';
 import Table from '@material-ui/core/Table';
@@ -14,32 +12,89 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import AddIcon from '@material-ui/icons/Add';
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
+import Tooltip from '@material-ui/core/Tooltip';
 import styles from 'dan-components/Tables/tableStyle-jss';
+import qs from 'qs';
+import { makeSecureDecrypt } from '../../Helpers/security';
+import formatDate from '../../Helpers/formatDate';
+import { Button } from '@material-ui/core';
 
-let id = 0;
-function createData(campaigns, start_date, views) {
-  id += 1;
+function createData(id, campaigns, start_date, end_date, views) {
   return {
     id,
     campaigns,
     start_date,
-    views,
+    end_date,
+    views
   };
 }
 
-const data = [
-  createData('Campaign 1', '01-January-2020', '14k'),
-  createData('Campaign 2', '02-January-2020', '18k'),
-  createData('Campaign 3', '04-January-2020', '13k'),
-  createData('Campaign 4', '05-January-2020', '35k'),
-  createData('Campaign 5', '01-February-2020', '24k'),
-];
+async function getData(url, data) {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: qs.stringify(data)
+  });
+
+  return await response.json();
+}
+
+let campaignData = [];
 
 class MiniCampaignTable extends React.Component {
-
   state = {
     redirect: false,
+    isCampaigns: false,
+    open: false,
+    type: '',
+    cId: null,
+    page: 0,
+    rowsPerPage: 5,
+    redirectDetails: false
   }
+
+  handleChangePage = (event, page) => {
+    this.setState({ page });
+  };
+
+  handleChangeRowsPerPage = event => {
+    this.setState({ rowsPerPage: event.target.value });
+  };
+
+  componentDidMount() {
+    const user = JSON.parse(
+      makeSecureDecrypt(localStorage.getItem('user'))
+    );
+
+    const data = {
+      client_id: user.id
+    };
+
+    getData(`${API_URL}/campaign/client/ongoing-campaigns`, data)
+      .then((res) => {
+        if (res.status === 1) {
+          if (res.data.length > 0) {
+            let tempData = [];
+            res.data.map(item => {
+              item.views = '0k';
+              const createDate = formatDate(item.created_at);
+              const deadline = formatDate(item.deadline);
+              tempData.push(createData(item.id, item.campaign_name, createDate, deadline, item.views));
+            });
+            campaignData = tempData;
+            this.setState({ isCampaigns: true });
+          }
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   setRedirect = () => {
     this.setState({
       redirect: true,
@@ -51,13 +106,29 @@ class MiniCampaignTable extends React.Component {
       return <Redirect to='/client/campaigns' />
     }
   }
+
+  setDetailsRedirect = (e, id) => {
+    this.setState({
+      redirectDetails: true,
+      cId: id
+    });
+  }
+
+  renderDetailsRedirect = () => {
+    if (this.state.redirectDetails) {
+      return <Redirect to={`/client/campaign-details/${this.state.cId}`} />;
+    }
+  }
+
   render() {
     const { classes } = this.props;
+    const { isCampaigns, open, type, rowsPerPage, page } = this.state;
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, campaignData.length - (page * rowsPerPage));
 
     return (
       <Fragment>
         {this.renderRedirect()}
-
+        {this.renderDetailsRedirect()}
         <div className={classes.rootTable}>
           <Toolbar className={classes.toolbar}>
             <div className={classes.title}>
@@ -73,24 +144,55 @@ class MiniCampaignTable extends React.Component {
               </Tooltip>
             </div>
           </Toolbar>
-          <Table className={classNames(classes.table, classes.hover)}>
-            <TableHead>
-              <TableRow>
-                <TableCell padding="default">Campaign Name</TableCell>
-                <TableCell align="left">Start Date</TableCell>
-                <TableCell align="left">Views</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {data.map(n => ([
-                <TableRow key={n.id}>
-                  <TableCell padding="default">{n.campaigns}</TableCell>
-                  <TableCell align="left">{n.start_date}</TableCell>
-                  <TableCell align="left">{n.views}</TableCell>
-                </TableRow>
-              ]))}
-            </TableBody>
-          </Table>
+          {isCampaigns
+            ? (
+              <Table className={classNames(classes.table, classes.hover)}>
+                <TableHead>
+                  <TableRow>
+                    <TableCell padding="default">Campaign Name</TableCell>
+                    <TableCell align="left">Start Date</TableCell>
+                    <TableCell align="left">End Date</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {campaignData.slice(page * rowsPerPage, (page * rowsPerPage) + rowsPerPage).map(n => (
+                    <TableRow key={n.id} onClick={(e) => this.setDetailsRedirect(e, n.id)}>
+                      <TableCell padding="default">{n.campaigns}</TableCell>
+                      <TableCell align="left">{n.start_date}</TableCell>
+                      <TableCell align="left">{n.end_date}</TableCell>
+                    </TableRow>
+                  ))}
+                  {emptyRows > 0 && (
+                    <TableRow style={{ height: 48 * emptyRows }}>
+                      <TableCell colSpan={6} />
+                    </TableRow>
+                  )}
+                </TableBody>
+                <TableFooter>
+                  <TableRow>
+                    <TablePagination
+                      colSpan={3}
+                      count={campaignData.length}
+                      rowsPerPage={rowsPerPage}
+                      rowsPerPageOptions={[5, 10, 15]}
+                      page={page}
+                      onChangePage={this.handleChangePage}
+                      onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                    />
+                  </TableRow>
+                </TableFooter>
+              </Table>
+            )
+            : (
+              <Typography
+                color="textSecondary"
+                variant="body1"
+                className={classes.warnMsg}
+              >
+                No Recent campaigns !
+              </Typography>
+            )
+          }
         </div>
       </Fragment>
     );
